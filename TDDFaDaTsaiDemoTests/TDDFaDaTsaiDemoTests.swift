8021ae71248df1edc912965c769c953756f5f873
DinDin <din.din@appimc.com>
2019-08-20 15:50:58 +0800

    [Update] update icon & clean code

--- TDDFaDaTsaiDemoTests/TDDFaDaTsaiDemoTests.swift
+++ TDDFaDaTsaiDemoTests/TDDFaDaTsaiDemoTests.swift
@@ -25,17 +25,7 @@ class TDDFaDaTsaiDemoTests: XCTestCase {
         XCTAssert(faTsaiLa.checkCount(6))
         
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
