622d278ce349634e86b30e5cf661ed8fe68f6f29
DinDin <din.din@appimc.com>
2019-08-20 10:32:28 +0800

    Initial Commit

---
+++ TDDFaDaTsaiDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TDDFaDaTsaiDemo
+//
+//  Created by DinDin on 2019/8/20.
+//  Copyright © 2019 DinDin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
