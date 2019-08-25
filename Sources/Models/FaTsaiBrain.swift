d62a2c49307ecd6759c3a2d13517a25930f07711
DinDin <din.din@appimc.com>
2019-08-25 16:42:13 +0800

    [Update] move FaTsaiBrain

--- Sources/Models/FaTsaiBrain.swift
+++ Sources/Models/FaTsaiBrain.swift
@@ -7,3 +7,62 @@
 //
 
 import Foundation
+
+class FaTsaiBrain {
+    
+    private(set) var list: [Int] = []
+    
+    init(_ list: [Int]) {
+        self.list = list
+    }
+    
+    func updateList(_ list: [Int]) {
+        self.list = list
+    }
+    
+    func checkNumbersCount(_ count: Int) -> Bool {
+        guard self.list.count == count else {
+            return false
+        }
+        return true
+    }
+    
+    func checkNumbersRange(first: Int, last: Int) -> Bool {
+        //待實作
+        for number in list {
+            if first > number {
+                return false
+            }
+            if last < number {
+                return false
+                
+            }
+        }
+        return true
+    }
+    
+    //    func checkNumbersAreOdd()-> Bool {
+    //        //待實作
+    //        return false
+    //    }
+    //
+    //    func checkNumbersAreEven()-> Bool {
+    //        //待實作
+    //        return false
+    //    }
+    
+    func checkNumbersContainSpecificNumbers(_ numbers: [Int])-> Bool {
+        //待實作
+        return false
+    }
+    
+    func checkNumbersNotsContainSpecificNumbers(_ numbers: [Int])-> Bool {
+        //待實作
+        return false
+    }
+    
+    func checkNumbersAreContinuitys(_ number: Int)-> Bool {
+        //待實作
+        return false
+    }
+}
